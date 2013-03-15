ce591a2b0adc2214ef03ff84a11f55bd2306ddf1
itandt <automatix@itandt.com>
2013-03-15 15:30:58 +0100

    syntax errors fixed in the weekdays SQL files #5

--- config/database/unisportr-basicdata_weekdays.sql
+++ config/database/unisportr-basicdata_weekdays.sql
@@ -1,8 +1,8 @@
 -- weekdays
-INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (1, 'Monaday', 'Mon', 'Montag', 'Mo'),
-INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (2, 'Tuesday', 'Tue', 'Dienstag', 'Di'),
-INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (3, 'Wednesday', 'Wed', 'Mittwoch', 'Mi'),
-INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (4, 'Thursday', 'Thu', 'Donnerstag', 'Do'),
-INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (5, 'Friday', 'Fri', 'Freitag', 'Fr'),
-INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (6, 'Saturday', 'Sat', 'Samstag', 'Sa'),
+INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (1, 'Monaday', 'Mon', 'Montag', 'Mo');
+INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (2, 'Tuesday', 'Tue', 'Dienstag', 'Di');
+INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (3, 'Wednesday', 'Wed', 'Mittwoch', 'Mi');
+INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (4, 'Thursday', 'Thu', 'Donnerstag', 'Do');
+INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (5, 'Friday', 'Fri', 'Freitag', 'Fr');
+INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (6, 'Saturday', 'Sat', 'Samstag', 'Sa');
 INSERT IGNORE INTO `weekdays` (`id`, `labelen`, `shortlabelen`, `labelde`, `shortlabelde`) VALUES (7, 'Sunday', 'Sun', 'Sonntag', 'So');
