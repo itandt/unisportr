18dd03801203d8370a5c06530e2d05f338cfa4cd
itandt <automatix@itandt.com>
2013-03-18 21:42:18 +0100

    Migration mechanism extended with dummy data import. Dummy data SQL file created and set to the Git ignore list. CLOSES #6

--- temp/simpledbmigration/functions.php
+++ temp/simpledbmigration/functions.php
@@ -31,6 +31,26 @@ function executeSQLFiles(array $dbOptions, array $dbFiles) {
 			echo $listItem['file'] . ': ' . mysqli_error($dbConnection) . PHP_EOL;
 		}
 	}
+	// db dummy data
+	foreach ($dbFiles['dummydata'] as $listItem) {
+		$query = file_get_contents(__DIR__ . '/../../data/dumps/' . $listItem['file']);
+		if (mysqli_multi_query($dbConnection, $query)) {
+			do {
+				$result = mysqli_store_result($dbConnection);
+				if (mysqli_connect_error($dbConnection)) {
+					var_dump($result);
+					echo $listItem['file'] . ': ' . mysqli_error($dbConnection) . PHP_EOL;
+				} else {
+					// echo $listItem['file'] . ' ' . 'OK' . PHP_EOL;
+				}
+				if (mysqli_more_results($dbConnection)) {
+					// echo '-----------------' . PHP_EOL;
+				}
+			} while (mysqli_next_result($dbConnection));
+		} else {
+			echo $listItem['file'] . ': ' . mysqli_error($dbConnection) . PHP_EOL;
+		}
+	}
 	// db migration
 	// close connection
 	mysqli_close($dbConnection);
--- temp/simpledbmigration/main.php
+++ temp/simpledbmigration/main.php
@@ -2,6 +2,7 @@
 $dbOptions = include __DIR__ . '/config.php';
 $dbFiles = array();
 $dbFiles['setup'] = include __DIR__ . '/../../config/dbsetup.php';
+$dbFiles['dummydata'] = include __DIR__ . '/../../config/dummydata.php';
 $dbFiles['migration'] = include __DIR__ . '/../../config/dbmigration.php';
 include __DIR__ . '/functions.php';
 
