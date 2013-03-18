f41c3d5be5bb390f390187f29f2f3deef143d67d
itandt <automatix@itandt.com>
2013-03-18 21:35:42 +0100

    some typos #5

--- config/dbmigration.php
+++ config/dbmigration.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * List of all migration-files in sorted order.
- * The Files have to be storen in /module/%module-name%/database/.
+ * The files have to be stored in /module/%module-name%/database/.
  *
  * - Add your entry at the end of this file!
  * - Rule: Every entry uses one line. This is important for merges.
--- config/dbsetup.php
+++ config/dbsetup.php
@@ -2,7 +2,7 @@
 /**
  * List of all files to set up the DB in sorted order.
  * Works similar to the DB migration script.
- * The Files have to be storen in /config/database/.
+ * The files have to be stored in /config/database/.
  *
  * - Add your entry at the end of this file!
  * - Rule: Every entry uses one line. This is important for merges.
