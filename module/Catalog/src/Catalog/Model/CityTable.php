2a70b788282f94c6cd55615ee30edbd14bb6d644
itandt <automatix@itandt.com>
2013-03-25 19:13:35 +0100

    Yet needless method CityTyble#getCity() removed.

--- module/Catalog/src/Catalog/Model/CityTable.php
+++ module/Catalog/src/Catalog/Model/CityTable.php
@@ -16,14 +16,4 @@ class CityTable {
 		return $resultSet;
 	}
 	
-	public function getCity($id) {
-		$id = (int) $id;
-		$rowset = $this->tableGateway->select(array('id' => $id));
-		$row = $rowset->current();
-		if (!$row) {
-			throw new \Exception('Could not find row ' . $id);
-		}
-		return $row;
-	}
-	
 }
