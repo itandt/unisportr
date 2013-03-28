76a0326290a5f2e4f3044fffea07588430a608f2
itandt <automatix@itandt.com>
2013-03-28 16:35:30 +0100

    Folder '/dev' added -- for miscellaneous data for development.

---
+++ dev/list-sports.sql
@@ -0,0 +1,19 @@
+SELECT
+	sports.id,
+	sports.title,
+	sports.category,
+	courses.id AS course_id,
+	COUNT(courses.id) AS count_courses
+FROM
+	sports
+	JOIN courses_sports ON courses_sports.sport_id = sports.id
+	JOIN courses ON courses_sports.course_id = courses.id
+	JOIN allproviders ON courses.provider_id = allproviders.id
+	JOIN cities ON cities.id = allproviders.city_id
+WHERE
+	courses.startdate < NOW()
+	AND courses.enddate > NOW()
+	AND cities.name = 'München'
+GROUP BY
+	sports.id,
+	sports.category
