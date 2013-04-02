06ed4694156ee2490c8501e9966971092202ee13
itandt <automatix@itandt.com>
2013-04-02 20:11:57 +0200

    Encoding settings for the database adapter added and commented out.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -17,6 +17,7 @@ return array(
 			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 		),
 		'pdodriver'      => 'mysql',
+		// 'driver_options' => array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"),
 	),
 	'service_manager' => array(
 		'factories' => array(
