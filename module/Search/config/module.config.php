6ced1967767b2f6730008a9d5ef38cb41dd0cb77
itandt <automatix@itandt.com>
2013-04-03 15:13:26 +0200

    Router configured for Search module. #17

--- module/Search/config/module.config.php
+++ module/Search/config/module.config.php
@@ -1,3 +1,28 @@
 <?php
 return array(
+	'controllers' => array(
+		'invokables' => array(
+			'Search\Controller\Search' => 'Search\Controller\SearchController',
+		),
+	),
+	'router' => array(
+		'routes' => array(
+			'search-courses' => array(
+				'type'	=> 'literal',
+				'options' => array(
+					'route'	=> '/search/courses',
+					'defaults' => array(
+						'controller' => 'Search\Controller\Search',
+						'action'	 => 'search-courses',
+					),
+				),
+				'may_terminate' => true,
+			),
+		),
+	),
+	'view_manager' => array(
+		'template_path_stack' => array(
+			'course' => __DIR__ . '/../view',
+		),
+	),
 );
