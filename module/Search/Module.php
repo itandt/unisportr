e4394b54d7656a12eee56d265c42da056da936df
itandt <automatix@itandt.com>
2013-04-03 12:06:00 +0200

    basic setup for the Search module class. #17

--- module/Search/Module.php
+++ module/Search/Module.php
@@ -1,21 +1,36 @@
 <?php
 namespace Search;
 
-class Module
-{
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
+class Module implements ConfigProviderInterface, ServiceProviderInterface, AutoloaderProviderInterface {
+	
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
 
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\ClassMapAutoloader' => array(
+				__DIR__ . '/autoload_classmap.php',
+			),
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+	
+	public function getServiceConfig() {
+		try {
+			return array (
+				'factories' => array(
+				)
+			);
+		} catch (\Exception $e) {
+			do {
+				echo $e->getMessage();
+			} while ($e = $e->getPrevious());
+		}
+	}
+	
 }
