a8a98fa3357e28834f8ec07fafd12ec62531d0f7
itandt <automatix@itandt.com>
2013-04-09 18:51:32 +0200

    Application module router changed for satic pages. Application\IndexController extended with some actions for 'about' and 'musthead'. Views for these static pages added. #21

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -27,7 +27,7 @@ return array(
             'application' => array(
                 'type'    => 'Literal',
                 'options' => array(
-                    'route'    => '/application',
+                    'route'    => '/',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
                         'controller'    => 'Index',
@@ -39,9 +39,8 @@ return array(
                     'default' => array(
                         'type'    => 'Segment',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
+                            'route'    => '[:action]',
                             'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
                             'defaults' => array(
--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -12,10 +12,18 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
+class IndexController extends AbstractActionController {
+	
+    public function indexAction() {
         return new ViewModel();
     }
+	
+    public function mustheadAction() {
+        return new ViewModel();
+    }
+	
+    public function aboutAction() {
+        return new ViewModel();
+    }
+    
 }
