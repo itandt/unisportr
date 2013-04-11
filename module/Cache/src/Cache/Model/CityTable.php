9562ccc82249a343a2981dcd36dab3b8a355e898
itandt <automatix@itandt.com>
2013-04-11 18:49:11 +0200

    Only cities with courses are selected now for the catalog, the city dropdown at the search form, and on the /about page.

--- module/Cache/src/Cache/Model/CityTable.php
+++ module/Cache/src/Cache/Model/CityTable.php
@@ -4,6 +4,8 @@ namespace Cache\Model;
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Db\Sql\Where;
 use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Predicate\Predicate;
 
 class CityTable {
 	
@@ -17,9 +19,19 @@ class CityTable {
 	 * @return ResultSet
 	 */
 	public function fetchAll() {
+		$select = new Select();
 		$where = new Where();
+		$select->columns(array('id', 'name'));
+		$select->from($this->tableGateway->getTable());
+		$select
+			->join('allproviders', 'allproviders.city_id = cities.id', array())
+			->join('courses', 'courses.provider_id = allproviders.providerid', array())
+		;
 		$where->notEqualTo('name', '');
-		$resultSet = $this->tableGateway->select($where);
+		$where->isNull('courses.status');
+		$select->where($where, Predicate::OP_AND);
+		$select->quantifier(Select::QUANTIFIER_DISTINCT);
+		$resultSet = $this->tableGateway->selectWith($select);
 		return $resultSet;
 	}
 	
--- module/Catalog/src/Catalog/Model/CityTable.php
+++ module/Catalog/src/Catalog/Model/CityTable.php
@@ -3,6 +3,8 @@ namespace Catalog\Model;
 
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Db\Sql\Where;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Predicate\Predicate;
 
 class CityTable {
 	
@@ -13,9 +15,20 @@ class CityTable {
 	}
 	
 	public function fetchAll() {
+		$select = new Select();
 		$where = new Where();
+		$select->columns(array('id', 'name'));
+		$select->from($this->tableGateway->getTable());
+		$select
+			->join('allproviders', 'allproviders.city_id = cities.id', array())
+			->join('courses', 'courses.provider_id = allproviders.providerid', array())
+		;
 		$where->notEqualTo('name', '');
-		$resultSet = $this->tableGateway->select($where);
+		$where->isNull('courses.status');
+		$select->where($where, Predicate::OP_AND);
+		$select->quantifier(Select::QUANTIFIER_DISTINCT);
+		// $test = $select->getSqlString($this->tableGateway->getAdapter()->getPlatform());
+		$resultSet = $this->tableGateway->selectWith($select);
 		return $resultSet;
 	}
 	
