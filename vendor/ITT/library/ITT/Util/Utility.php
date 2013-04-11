148dc8ae78a7c8af3583c4f16a958927638157f4
itandt <automatix@itandt.com>
2013-04-11 20:41:30 +0200

    The view for the catalog lists: list-courses is completed. CLOSE #16

---
+++ vendor/ITT/library/ITT/Util/Utility.php
@@ -0,0 +1,22 @@
+<?php
+namespace ITT\Util;
+
+class Utility {
+	
+	public static function getOneOrPair($elementA, $elementB, $delimiter) {
+		$return = null;
+		if($elementA == $elementB) {
+			$return = $elementA;
+		} else {
+			if (empty($elementA)) {
+				$return = $elementB;
+			} else if (empty($elementB)) {
+				$return = $elementA;
+			} else {
+				$return = $elementA . $delimiter . $elementB;
+			}
+		}
+		return $return;
+	}
+	
+}
