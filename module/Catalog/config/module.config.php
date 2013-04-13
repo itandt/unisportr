3723beb132c1a018d6444905a0c7c22965f96093
itandt <automatix@itandt.com>
2013-04-13 05:16:49 +0200

    Bugfix: Cities with spaces, like Frankfurt am Main, can be reached now.

--- module/Catalog/config/module.config.php
+++ module/Catalog/config/module.config.php
@@ -21,7 +21,7 @@ return array(
 			'city' => array(
 				'type'	=> 'ITT\Mvc\Router\Http\UnicodeRegex',
 				'options' => array(
-					'regex'	=> '/catalog/(?<city>[\p{L}]*)',
+					'regex'	=> '/catalog/(?<city>[\p{L}\p{Zs}]*)',
 					'defaults' => array(
 						'controller' => 'Catalog\Controller\Catalog',
 						'action'	 => 'list-sports',
@@ -33,7 +33,7 @@ return array(
 			'sport' => array(
 				'type'	=> 'ITT\Mvc\Router\Http\UnicodeRegex',
 				'options' => array(
-					'regex'	=> '/catalog/(?<city>[\p{L}]*)/(?<sport>[\p{L}]*)',
+					'regex'	=> '/catalog/(?<city>[\p{L}\p{Zs}]*)/(?<sport>[\p{L}\p{Zs}]*)',
 					'defaults' => array(
 						'controller' => 'Catalog\Controller\Catalog',
 						'action'	 => 'list-courses',
