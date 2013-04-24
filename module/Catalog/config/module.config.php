453948cc77740e9a2c5dfe5f000897c3e5e144d4
itandt <automatix@itandt.com>
2013-04-24 18:59:31 +0200

    The regex of the sport route has been corrected (extended with punctuation). CLOSE #49

--- module/Catalog/config/module.config.php
+++ module/Catalog/config/module.config.php
@@ -33,7 +33,7 @@ return array(
 			'sport' => array(
 				'type'	=> 'ITT\Mvc\Router\Http\UnicodeRegex',
 				'options' => array(
-					'regex'	=> '/catalog/(?<city>[\p{L}\p{Zs}]*)/(?<sport>[\p{L}\p{Zs}]*)',
+					'regex'	=> '/catalog/(?<city>[\p{L}\p{Zs}]*)/(?<sport>[\p{L}\p{Zs}\p{P}]*)',
 					'defaults' => array(
 						'controller' => 'Catalog\Controller\Catalog',
 						'action'	 => 'list-courses',
