4f7399c425bcabb9a83401e47a41674f8dbc8d71
itandt <automatix@itandt.com>
2013-05-01 22:38:05 +0200

    HeadMeta view helper code improved.

--- vendor/ITT/library/ITT/View/Helper/HeadMeta.php
+++ vendor/ITT/library/ITT/View/Helper/HeadMeta.php
@@ -2,21 +2,27 @@
 namespace ITT\View\Helper;
 
 use stdClass;
-use \Zend\View;
-use \Zend\View\Exception;
+use Zend\View;
+use Zend\View\Exception;
 use Zend\View\Helper\HeadMeta as ZendHeadMeta;
 use Zend\View\Helper\Placeholder\Container\AbstractContainer;
 
 class HeadMeta extends ZendHeadMeta
 {
 	const DELIMITER = ' - ';
-	
+
+	/**
+	 * @see \Zend\View\Helper\HeadMeta::__invoke()
+	 */
 	public function __invoke($content = null, $keyValue = null, $keyType = 'name', $modifiers = array(), $placement = AbstractContainer::APPEND)
 	{
 		parent::__invoke($content, $keyValue, $keyType, $modifiers, $placement);
 		return $this;
 	}
-	
+
+	/**
+	 * @see \Zend\View\Helper\HeadMeta::append()
+	 */
 	public function append($value) {
 		if ($value->name == 'description') {
 			$this->updateDescription($value, AbstractContainer::APPEND);
@@ -26,7 +32,10 @@ class HeadMeta extends ZendHeadMeta
 			parent::append($value);
 		}
 	}
-	
+
+	/**
+	 * @see \Zend\View\Helper\HeadMeta::prepend()
+	 */
 	public function prepend($value) {
 		if ($value->name == 'description') {
 			$this->updateDescription($value, AbstractContainer::PREPEND);
@@ -37,7 +46,12 @@ class HeadMeta extends ZendHeadMeta
 		}
 	}
 	
-	// 
+	// Not working correctly!
+	// Can cause a
+	// Fatal error: Maximum function nesting level of '100' reached, aborting!
+	/**
+	 * @see \Zend\View\Helper\HeadMeta::set()
+	 */
 	public function set($value) {
 		if ($value->name == 'description') {
 			$this->updateDescription($value, AbstractContainer::SET);
@@ -48,7 +62,13 @@ class HeadMeta extends ZendHeadMeta
 		}
 	}
 	
-	public function updateDescription(\stdClass $value, $position = AbstractContainer::SET) {
+	/**
+	 * If a description meta already exsists, extends it with $value->content,
+	 * else creates a new desctiprion meta.
+	 * @param \stdClass $value
+	 * @param string $position
+	 */
+	public function updateDescription(stdClass $value, $position = AbstractContainer::SET) {
 		$descriptionExists = false;
 		foreach ($this->getContainer() as $item) {
 	 		if ($this->isDescription($item)) {
@@ -69,13 +89,28 @@ class HeadMeta extends ZendHeadMeta
 	 		}
 	 	}
 	 	if (!$descriptionExists) {
-	 		parent::append($value);
+	 		switch ($position) {
+	 			case AbstractContainer::APPEND:
+	 				parent::append($value);
+	 				break;
+	 			case AbstractContainer::PREPEND:
+	 				parent::prepend($value);
+	 				break;
+	 			case AbstractContainer::SET:
+	 			default:
+	 				parent::set($value);
+	 				break;
+	 		}
 	 	}
 	}
-
-	// Not working!
-	// Fatal error: Maximum function nesting level of '100' reached, aborting!
-	public function updateKeywords(\stdClass $value, $position = AbstractContainer::SET) {
+	
+	/**
+	 * If a keywords meta already exsists, extends it with $value->content,
+	 * else creates a new keywords meta.
+	 * @param \stdClass $value
+	 * @param string $position
+	 */
+	public function updateKeywords(stdClass $value, $position = AbstractContainer::SET) {
 		$keywordsExists = false;
 		foreach ($this->getContainer() as $item) {
 	 		if ($this->isKeywords($item)) {
@@ -97,9 +132,7 @@ class HeadMeta extends ZendHeadMeta
 		}
 	 	if (!$keywordsExists) {
 	 		parent::append($value);
-			$breakpoint = null;
 	 	}
-		$breakpoint = null;
 	}
 	
 // 	public function groupDescription() {
@@ -111,7 +144,7 @@ class HeadMeta extends ZendHeadMeta
 // 	 	}
 // 	 	// $this->getContainer()->offsetUnset(0);
 // 	 	if (!empty($descriptionContentSet)) {
-// 	 		$groupedDescription = new \stdClass();
+// 	 		$groupedDescription = new stdClass();
 // 	 		$groupedDescription->type = 'name';
 // 	 		$groupedDescription->name = 'description';
 // 	 		$groupedDescription->content = implode(', ', $descriptionContentSet);
@@ -128,7 +161,7 @@ class HeadMeta extends ZendHeadMeta
 // 	 		}
 // 	 	}
 // 	 	if (!empty($keywordsContentSet)) {
-// 	 		$groupedKeywords = new \stdClass();
+// 	 		$groupedKeywords = new stdClass();
 // 	 		$groupedKeywords->type = 'name';
 // 	 		$groupedKeywords->name = 'keywords';
 // 	 		$groupedKeywords->content = implode(', ', $keywordsContentSet);
@@ -165,6 +198,9 @@ class HeadMeta extends ZendHeadMeta
 // 		}
 //	 }
 	
+	/**
+	 * @return description meta text
+	 */
 	public function getDescription() {
 		$description = null;
 		foreach ($this->getContainer() as $item) {
@@ -175,7 +211,10 @@ class HeadMeta extends ZendHeadMeta
 		}
 		return $description;
 	}
-	
+
+	/**
+	 * @return keywords meta text
+	 */
 	public function getKeywords() {
 		$keywords = null;
 		foreach ($this->getContainer() as $item) {
@@ -187,6 +226,13 @@ class HeadMeta extends ZendHeadMeta
 		return $keywords;
 	}
 	
+	/**
+	 * Checks, whether the input $item is an approproate object for $this->container
+	 * and wheter it's a description object (its name is "description")
+	 * @param stdClass $item
+	 * @throws Exception\InvalidArgumentException
+	 * @return boolean
+	 */
 	public function isDescription(stdClass $item) {
 		if (!in_array($item->type, $this->typeKeys)) {
 			throw new Exception\InvalidArgumentException(sprintf(
@@ -197,6 +243,13 @@ class HeadMeta extends ZendHeadMeta
 		return $item->name == 'description';
 	}
 	
+	/**
+	 * Checks, whether the input $item is an approproate object for $this->container
+	 * and wheter it's a keywords object (its name is "keywords")
+	 * @param stdClass $item
+	 * @throws Exception\InvalidArgumentException
+	 * @return boolean
+	 */
 	public function isKeywords(stdClass $item) {
 		if (!in_array($item->type, $this->typeKeys)) {
 			throw new Exception\InvalidArgumentException(sprintf(
