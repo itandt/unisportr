3da50aa0d62b2b3b0d307aa07e6e324e39fc936f
itandt <automatix@itandt.com>
2013-10-17 00:06:27 +0200

    The "display course" view splitted into three ones: "course-info", "course-forbidden", "course-expired". CLOSES #72

--- module/Course/src/Course/Controller/CourseController.php
+++ module/Course/src/Course/Controller/CourseController.php
@@ -21,32 +21,12 @@ class CourseController extends AbstractActionController {
 		$id = $this->params()->fromRoute('id', null);
 		$title = $this->params()->fromRoute('title', null);
 		$course = $this->getCourseTable()->findOnceByID($id)->current();
-		
-		$dateTimeNow = new \DateTime('now');
-		$dateTimeCourseEndDate = new \DateTime($course->endDate);
-		
-		/*
-		$interval = $dateTimeNow->diff($dateTimeCourseEndDate);
-		echo
-			$dateTimeNow->format('d.m.Y') . ' | ' .
-			$dateTimeCourseEndDate->format('d.m.Y') . ' | ' .
-			$interval->format('%d') . ' || '
-		;
-		*/
-		if ($dateTimeCourseEndDate > $dateTimeNow) {
-			$result = new ViewModel(array(
-				'id' => $id,
-				'course' => $course,
-				'gMapsKey' => $this->getServiceLocator()->get('Config')['gMapsKey'],
-				'uri' => $this->getRequest()->getUri(),
-			));
-		} else {
-			$result = $this->forward()->dispatch('Course\Controller\Course', array(
-				'action' => 'course-expired',
-				'course' => $course,
-			));
-		}
-		
+		$result = new ViewModel(array(
+			'id' => $id,
+			'course' => $course,
+			'gMapsKey' => $this->getServiceLocator()->get('Config')['gMapsKey'],
+			'uri' => $this->getRequest()->getUri(),
+		));
 		return $result;
 	}
 	
